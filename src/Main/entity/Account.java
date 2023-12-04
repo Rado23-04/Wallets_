317f27e9f9fc19721e93c4bff9127a8b49d393aa
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-12-04 19:56:18 +0300

    feat: "add Account model"

---
+++ src/Main/entity/Account.java
@@ -0,0 +1,75 @@
+package Main.entity;
+
+import java.util.Date;
+
+public class Account {
+//     "id": 1,
+//             "pay": "Kevin",
+//             "account_type": "Savings",
+//             "currency_id": "Ariary",
+//             "create_date": "2023-01-01"
+    private int id;
+    private String pay;
+    private String accountType;
+    private int currencyId;
+    private Date createDate;
+
+    public Account(int id, String pay, String accountType, int currencyId, Date createDate) {
+        this.id = id;
+        this.pay = pay;
+        this.accountType = accountType;
+        this.currencyId = currencyId;
+        this.createDate = createDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPay() {
+        return pay;
+    }
+
+    public void setPay(String pay) {
+        this.pay = pay;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public int getCurrencyId() {
+        return currencyId;
+    }
+
+    public void setCurrencyId(int currencyId) {
+        this.currencyId = currencyId;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", pay='" + pay + '\'' +
+                ", accountType='" + accountType + '\'' +
+                ", currencyId=" + currencyId +
+                ", createDate=" + createDate +
+                '}';
+    }
+}
