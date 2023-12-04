7a31b02543ea914fc2996b17399e02b143ba85ec
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-12-04 20:21:21 +0300

    feat: "add CrudOperations.java"

---
+++ src/Main/generic/CrudOperations.java
@@ -0,0 +1,14 @@
+package Main.generic;
+
+import java.util.List;
+
+public interface CrudOperations<T> {
+    List<T> findAll();
+
+    List<T> saveAll(List<T> toSave);
+
+    T save(T toSave);
+
+    T delete(T toDelete);
+
+}
