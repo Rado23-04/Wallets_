8912f1ca6d3423ea774ee8a6870d889efb69123b
rado <hei.rado.2@gmail.com>
2023-12-04 21:26:22 +0300

    fix: completed operation for Account DAO

--- src/Main/generic/AccountDAO.java
+++ src/Main/generic/AccountDAO.java
@@ -64,17 +64,61 @@ public class AccountDAO implements CrudOperations<Account>{
 
     @Override
     public Account getById(Account getByid) {
+        String sql = "Select * from account where id =?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, getByid.getId());
+            ResultSet resultSet = preparedStatement.executeQuery();
 
+            if (resultSet.next()) {
+                String pay = resultSet.getString("pay");
+                String accountType = resultSet.getString("account_type");
+                int currencyId = resultSet.getInt("currency_id");
+                String createDate = resultSet.getString("create_date");
+
+                getByid.setPay(pay);
+                getByid.setAccountType(accountType);
+                getByid.setCurrencyId(currencyId);
+                getByid.setCreateDate(createDate);
+
+                return getByid;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Account delete(Account toDelete) {
+        String sql = "Delete * from Account where id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, toDelete.getId());
+            preparedStatement.executeUpdate();
+            return toDelete;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
     public Account put(Account put) {
+        String query = "UPDATE account SET pay = ?, account_type = ?, currency_id = ?, create_date = ? WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, put.getPay());
+            preparedStatement.setString(2, put.getAccountType());
+            preparedStatement.setInt(3, put.getCurrencyId());
+            preparedStatement.setString(4, put.getCreateDate());
+            preparedStatement.setInt(5, put.getId());
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                return put;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
